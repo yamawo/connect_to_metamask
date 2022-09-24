29f3911fb0f2c824f3574ab20d93ec38573ec8e0
yamawo <y7a3m1asaki@gmail.com>
2022-09-24 19:56:19 +0900

    fix: delete unnecessary import

--- lib/pages/login_page.dart
+++ lib/pages/login_page.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:flutter/services.dart' show rootBundle;
 import 'package:url_launcher/url_launcher.dart';
 import 'package:walletconnect_dart/walletconnect_dart.dart';
 
